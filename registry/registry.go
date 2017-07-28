1efc3d43d4d5a1309c529d55ed23710fe4e2b7c5
murongyulong <931357304@qq.com>
2017-07-28 10:45:35 +0800

    Update registry.go

--- registry/registry.go
+++ registry/registry.go
@@ -179,6 +179,7 @@ func (r *RouteRegistry) GenerateUriMap() (map[route.Uri]*route.Pool, bool) {
 			uri = uri.ToLower()
 
 			pool, found := byUriTmp[uri]
+			r.logger.Debug("pool:"+pool)
 			if !found {
 				pool = route.NewPool(r.reloadUriInterval / 5)
 				byUriTmp[uri] = pool

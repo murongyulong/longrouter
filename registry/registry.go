2b30d09b164bcf24b54d232261e770a3448e2f97
murongyulong <931357304@qq.com>
2018-06-08 11:04:31 +0800

    Update registry.go

--- registry/registry.go
+++ registry/registry.go
@@ -22,12 +22,12 @@ func InitRedisConnPool(c *config.Config) {
 		MaxIdle:     100,
 		IdleTimeout: 240 * time.Second,
 		Dial: func() (redis.Conn, error) {
-			//conn, err := redis.Dial("tcp", c.RedisServer, redis.DialPassword(c.Password))
-			//if err != nil {
-			//	return nil, err
-			//}
-			//return conn, err
-			return redis.Dial("tcp", c.RedisServer, redis.DialPassword(c.Password))
+			conn, err := redis.Dial("tcp", c.RedisServer, "abcd1234")
+			if err != nil {
+				return nil, err
+			}
+			return conn, err
+			//return redis.Dial("tcp", c.RedisServer, redis.DialPassword(c.Password))
 		},
 	}
 }
